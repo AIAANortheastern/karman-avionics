c95e38e47f17673cc6cc0028db7d23db7f8cc8bf
Andrew Kaster <andrewdkaster@gmail.com>
2017-03-16 19:58:49 -0400

    move timer_init before tc_write_clock_source as required by API

--- karman-avionics/karman-avionics/src/main.c
+++ karman-avionics/karman-avionics/src/main.c
@@ -25,8 +25,8 @@ __attribute__((OS_main)) int main(void)
 
     /* Enable global interrupts and enable timer */
     cpu_irq_enable();
-    tc_write_clock_source(&TCC0, TC_CLKSEL_DIV1_gc);
     timer_init(); /* Initialize timer. DO NO REMOVE*/
+    tc_write_clock_source(&TCC0, TC_CLKSEL_DIV1_gc);
 
     /* TODO x a million. Need to put code in this function*/
     board_init(); /* Do board initialization steps. */
