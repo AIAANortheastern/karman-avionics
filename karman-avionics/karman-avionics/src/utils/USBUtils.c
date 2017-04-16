88cdc09efc9c18ad1583d5d5fb0ade699bda7cc7
Andrew Kaster <kaster.a@husky.neu.edu>
2017-04-16 16:16:42 -0400

    expand usb message/packet definitions

--- karman-avionics/karman-avionics/src/utils/USBUtils.c
+++ karman-avionics/karman-avionics/src/utils/USBUtils.c
@@ -10,50 +10,53 @@
 /* Computes checksum for message and fills packet pointer */
 Bool usb_utils_create_packet(uint16_t id, uint16_t len, uint8_t *message, usb_packet_t *packet)
 {
-	Bool retVal = false; /* Nothing wrong */
+    Bool retVal = false; /* Nothing wrong */
 
-	packet->hdr.packet_id = id;
-	packet->hdr.packet_len = len;
-	packet->message = message;
-	retVal = usb_utils_calculate_checksum(&(packet->checksum), message, len);
-
-	return retVal;
+    if(id < NUM_USB_MSG_ID)
+    {
+        packet->hdr.packet_magic = 0xDEADBEEF;
+        packet->hdr.packet_id = id;
+        packet->hdr.message_len = len;
+        packet->message = message;
+        retVal = usb_utils_calculate_checksum(&(packet->checksum), message, len);
+    }
+    else
+    {
+        retVal = true;
+    }
+    return retVal;
 }
 
 /* Takes in message and computes checksum */
 Bool usb_utils_calculate_checksum(uint16_t *checksum, uint8_t *message, uint16_t len)
 {
-	Bool retVal = false;
-	uint16_t word_idx = 0;
-	uint8_t byte_idx = 0;
+    Bool retVal = false;
+    uint16_t word_idx = 0;
+    uint8_t byte_idx = 0;
 
-	checksum = 0;
+    checksum = 0;
 
-	if(len < 1)
-	{
-		retVal = true;
-		*checksum = 0xFFFF;
-	}
-	else if(len == 1)
-	{
-		*checksum = (((uint16_t)(message[0])) << 8); 
-	}
-	else /* Message length greater than 0 */
-	{
-		/* Iterate over 2 byte words */
-		while(word_idx < (len >> 1))
-		{
-			*checksum += ((uint16_t *)message)[word_idx];
-			word_idx++;
-		}
-		/* handle last word if odd number of bytes*/
-		if(len & 1) /* if(len%2 == 1) */
-		{
-			byte_idx = len - 1;
-			*checksum+= (((uint16_t)(message[byte_idx])) << 8); 
-		}
-	}
+    if(len < 1)
+    {
+        retVal = true;
+        *checksum = 0xFFFF;
+    }
+    else /* Message length greater than 0 */
+    {
+        /* Iterate over 2 byte words */
+        while(word_idx < (len >> 1))
+        {
+            *checksum += ((uint16_t *)message)[word_idx];
+            word_idx++;
+        }
+        /* handle last word if odd number of bytes*/
+        if(len & 1) /* if(len%2 == 1) */
+        {
+            byte_idx = len - 1;
+            *checksum+= (((uint16_t)(message[byte_idx])) << 8); 
+        }
+    }
 
-	return retVal;
+    return retVal;
 }
 
--- karman-avionics/karman-avionics/src/utils/USBUtils.h
+++ karman-avionics/karman-avionics/src/utils/USBUtils.h
@@ -13,16 +13,83 @@
 
 typedef struct
 {
-	uint16_t packet_id;
-	uint16_t packet_len;
+    uint32_t packet_magic;
+    uint16_t packet_id;
+    uint16_t message_len;
 } usb_packet_header_t;
+
+#define USB_PACKET_HDR_SIZE (8)
 
 typedef struct
 {
-	usb_packet_header_t hdr;
-	uint8_t *message;
-	uint16_t checksum;
+    usb_packet_header_t hdr;
+    uint8_t *message;
+    uint16_t checksum;
 } usb_packet_t;
+
+/* A nack message will be sent to the host in the event that */
+/* a packet is recieved in an unexpected order, or with anything invalid */
+/* in the message (checksum, id, payload value, etc) */
+
+typedef enum 
+{
+    USB_ID_INIT = 0,
+    USB_ID_INIT_ACK,
+    USB_ID_REQ_MODE,
+    USB_ID_RECV_MODE,
+    USB_ID_ACK_MODE,
+    USB_ID_ACK_MODE_RESP,
+    USB_ID_EJTEST_MAIN,
+    USB_ID_EJTEST_DROG,
+    USB_ID_EJTEST_END,
+    USB_ID_MSG_NACK,
+    NUM_USB_MSG_ID,
+} usb_id_t;
+
+/* Initialization string to confirm with host that usb serial is working */
+#define USB_INIT_STR_SIZE (16)
+#define USB_INIT_STR ("The quick brown")
+
+typedef struct 
+{
+    char init_str[USB_INIT_STR_SIZE];
+} usb_msg_init_t;
+
+
+/* Expected return string from host after sending initialization string */
+#define USB_INIT_ACK_STR_SIZE (16)
+#define USB_INIT_ACK_STR ("fox jumped over")
+
+typedef struct
+{
+    char init_ack_str[USB_INIT_ACK_STR_SIZE];
+} usb_msg_init_ack_t;
+
+/* Instructs host to display CLI prompt that asks the user to select a mode */
+#define USB_REQ_MODE_SIZE (2)
+#define USB_REQ_MODE_MAGIC (0xCAFE) 
+
+typedef struct
+{
+    uint16_t mode_req_magicnum;
+} usb_msg_req_mode_t;
+
+/* Message from host to MCU containing the requested mode */
+#define USB_RECV_MODE_SIZE (2)
+
+typedef enum
+{
+    USB_EXEC_MODE_DACQ = 0x111,
+    USB_EXEC_MODE_DNLD = 0x222,
+    USB_EXEC_MODE_EJTEST = 0x999,
+} usb_execution_mode_t;
+
+typedef struct
+{
+    uint16_t execution_mode;
+} usb_msg_recv_mode;
+
+/* TODO The rest of the message definitions */
 
 /* Computes checksum for message and fills packet pointer */
 Bool usb_utils_create_packet(uint16_t id, uint16_t len, uint8_t *message, usb_packet_t *packet);
